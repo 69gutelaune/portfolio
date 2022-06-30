4f69b0f6257112e597a959ebb5777b58947a1248
Georgiy S <draw2know@icloud.com>
2022-06-30 15:39:45 +0200

    generating random letter

--- scripts/matrix_hero.js
+++ scripts/matrix_hero.js
@@ -6,7 +6,45 @@ canvas.height = window.innerHeight;
 
 const symbolsArray =
   "♫♪アァカサタナハマヤャラワンわワヰヱヲらりるれろラリルレロヤユヨマミムメモはひふへほハヒフヘいうえアイウエオかたにナニヌネノちつてとタチツテトきくけこカキクケコさしすせそサスセソABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
-let randomCharacter = symbolsArray.charAt(
-  Math.floor(Math.random() * symbolsArray.length)
-);
-console.log(randomCharacter);
+
+const fontSize = 25;
+
+// Column: create an Object function that draws vertical Letters
+//
+class Letter {
+  constructor(x, y) {
+    this.character = symbolsArray.charAt(
+      Math.floor(Math.random() * symbolsArray.length)
+    );
+    this.x = x;
+    this.y = y;
+  }
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.font = fontSize + "px monospace";
+    ctx.fillText(this.character, this.x, this.y);
+    console.log(this.character);
+  }
+}
+
+let newLetter = new Letter(100, 100);
+newLetter.draw();
+class Column {}
+//
+//
+// Row: create an Object or function that draws multiple lines horizontly
+//
+//
+//
+// create a function that reduces opacity of old letters
+//
+//
+//
+// ctrate a function that listens to resizing the window
+window.addEventListener("resize", () => {});
+
+// create gradients and colors
+//
+//
+//
